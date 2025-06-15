1a381ab61c132618630f440932614684103246e9
Gabriel <gabriel.gracilianomb@gmail.com>
2025-06-15 20:14:48 -0300

    Create template for board

---
+++ src/br/com/gracilianoog/util/BoardTemplate.java
@@ -0,0 +1,17 @@
+package br.com.gracilianoog.util;
+
+public final class BoardTemplate {
+    public final static String BOARD_TEMPLATE = """
+            %s %s %s | %s %s %s | %s %s %s
+            %s %s %s | %s %s %s | %s %s %s
+            %s %s %s | %s %s %s | %s %s %s
+            ---------|----------|---------
+            %s %s %s | %s %s %s | %s %s %s
+            %s %s %s | %s %s %s | %s %s %s
+            %s %s %s | %s %s %s | %s %s %s
+            ---------|----------|---------
+            %s %s %s | %s %s %s | %s %s %s
+            %s %s %s | %s %s %s | %s %s %s
+            %s %s %s | %s %s %s | %s %s %s
+            """;
+}
