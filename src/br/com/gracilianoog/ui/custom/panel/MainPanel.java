5d6177cdcd542ebc0b44df0d21cce9f27427758c
Gabriel <gabriel.gracilianomb@gmail.com>
2025-06-16 10:54:05 -0300

    Create main panel

---
+++ src/br/com/gracilianoog/ui/custom/panel/MainPanel.java
@@ -0,0 +1,11 @@
+package br.com.gracilianoog.ui.custom.panel;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainPanel extends JPanel {
+    public MainPanel(Dimension dimension) {
+        this.setSize(dimension);
+        this.setPreferredSize(dimension);
+    }
+}
