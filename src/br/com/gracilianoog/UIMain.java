53bd8252fd52a16d122c9d4d00d2172e6862da31
Gabriel <gabriel.gracilianomb@gmail.com>
2025-06-16 12:31:38 -0300

    Use MainScreen to build screen

--- src/br/com/gracilianoog/UIMain.java
+++ src/br/com/gracilianoog/UIMain.java
@@ -1,17 +1,20 @@
 package br.com.gracilianoog;
 
-import br.com.gracilianoog.ui.custom.frame.MainFrame;
-import br.com.gracilianoog.ui.custom.panel.MainPanel;
+import br.com.gracilianoog.ui.custom.screen.MainScreen;
 
-import javax.swing.*;
-import java.awt.*;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toMap;
 
 public class UIMain {
     public static void main(String[] args) {
-        Dimension dimension = new Dimension(600, 600);
-        JPanel mainPanel = new MainPanel(dimension);
-        JFrame mainFrame = new MainFrame(dimension, mainPanel);
-        mainFrame.revalidate();
-        mainFrame.repaint();
+        final Map<String, String> gameConfig = Stream.of(args).collect(toMap(
+                k -> k.split(";")[0],
+                v -> v.split(";")[1]
+        ));
+
+        MainScreen mainScreen = new MainScreen(gameConfig);
+        mainScreen.buildMainScreen();
     }
 }
