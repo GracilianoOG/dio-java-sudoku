a4b0758ca39ffa00ed415add6d7d0bbc9f98e3dd
Gabriel <gabriel.gracilianomb@gmail.com>
2025-06-16 12:30:45 -0300

    Add textfields for each sudoku sector

--- src/br/com/gracilianoog/ui/custom/panel/SudokuSector.java
+++ src/br/com/gracilianoog/ui/custom/panel/SudokuSector.java
@@ -1,15 +1,19 @@
 package br.com.gracilianoog.ui.custom.panel;
 
+import br.com.gracilianoog.ui.custom.input.NumberText;
+
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 import java.awt.*;
+import java.util.List;
 
 public class SudokuSector extends JPanel {
-    public SudokuSector() {
+    public SudokuSector(List<NumberText> textFields) {
         Dimension dimension = new Dimension(170, 170);
         this.setSize(dimension);
         this.setPreferredSize(dimension);
         this.setBorder(new LineBorder(Color.black, 2, true));
         this.setVisible(true);
+        textFields.forEach(this::add);
     }
 }
