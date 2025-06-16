c65616fdf011c6abf0f4b7ca4dbc21b8118d5b00
Gabriel <gabriel.gracilianomb@gmail.com>
2025-06-16 13:01:34 -0300

    Generate visible cells

--- src/br/com/gracilianoog/ui/custom/screen/MainScreen.java
+++ src/br/com/gracilianoog/ui/custom/screen/MainScreen.java
@@ -1,15 +1,20 @@
 package br.com.gracilianoog.ui.custom.screen;
 
+import br.com.gracilianoog.model.Cell;
 import br.com.gracilianoog.model.GameStatus;
 import br.com.gracilianoog.service.BoardService;
 import br.com.gracilianoog.ui.custom.button.CheckGameStatusButton;
 import br.com.gracilianoog.ui.custom.button.FinishGameButton;
 import br.com.gracilianoog.ui.custom.button.ResetButton;
 import br.com.gracilianoog.ui.custom.frame.MainFrame;
+import br.com.gracilianoog.ui.custom.input.NumberText;
 import br.com.gracilianoog.ui.custom.panel.MainPanel;
+import br.com.gracilianoog.ui.custom.panel.SudokuSector;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class MainScreen {
@@ -28,11 +33,36 @@ public class MainScreen {
     public void buildMainScreen() {
         JPanel mainPanel = new MainPanel(dimension);
         JFrame mainFrame = new MainFrame(dimension, mainPanel);
+        for (int row = 0; row < 9; row += 3) {
+            int endRow = row + 2;
+
+            for (int col = 0; col < 9; col += 3) {
+                int endCol = col + 2;
+                var cells = getCellsFromSector(boardService.getCells(), col, endCol, row, endRow);
+                JPanel sector = generateCells(cells);
+                mainPanel.add(sector);
+            }
+        }
         addFinishGameButton(mainPanel);
         addCheckGameStatusButton(mainPanel);
         addResetButton(mainPanel);
         mainFrame.revalidate();
         mainFrame.repaint();
+    }
+
+    private JPanel generateCells(List<Cell> cells) {
+        List<NumberText> fields = new ArrayList<>(cells.stream().map(NumberText::new).toList());
+        return new SudokuSector(fields);
+    }
+
+    private List<Cell> getCellsFromSector(List<List<Cell>> cells, int initCol, int endCol, int initRow, int endRow) {
+        List<Cell> cellSectors = new ArrayList<>();
+        for (int row = initRow; row <= endRow; row++) {
+            for (int col = initCol; col <= endCol; col++) {
+                cellSectors.add(cells.get(col).get(row));
+            }
+        }
+        return cellSectors;
     }
 
     private void addFinishGameButton(JPanel mainPanel) {
